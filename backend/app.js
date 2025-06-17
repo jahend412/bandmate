bf71d872da0871dfc3ec2cebbaea5ea45292b501
Josh Endemann <jahend44@gmail.com>
2025-06-16 20:31:37 -0500

    feat:  Add Test db route to verify it works
    -  New to me is the use of Pool.  Pool is used to manage mulitple database connections.
    -  SELECT NOW() is a simple SQL that returns the date and time

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,36 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(200).send("Server is up and running");
+});
+
+// Test the postgres Database
+app.get("/test-db", async (req, res) => {
+  try {
+    const { Pool } = require("pg");
+
+    const pool = new Pool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 5432,
+    });
+
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      success: true,
+      message: "Database connected!",
+      timestamp: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Database connection failed",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = app;
