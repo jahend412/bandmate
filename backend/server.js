4cec2e46912174f141e05d0ae6eb64be3bafd96f
Josh Endemann <jahend44@gmail.com>
2025-06-16 20:29:33 -0500

    add dotenv.config() to process from my env file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const app = require("./app.js");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
