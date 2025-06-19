3af4c71ca056db65b805225d0550f09c830fcc53
Josh Endemann <jahend44@gmail.com>
2025-06-18 19:27:16 -0500

    Add Login Route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,7 +9,7 @@ router.post("/register"),
     try {
       const { email, password, role } = req.body;
 
-      const existingUser = await pool.Query(
+      const existingUser = await pool.query(
         "SELECT * FROM users WHERE email = $1",
         [email]
       );
@@ -29,7 +29,7 @@ router.post("/register"),
       const hashedPassword = await bcrypt.hash(password, saltRounds);
 
       // Create user
-      const newUser = await pool.Query(
+      const newUser = await pool.query(
         "INSERT INTO  users  (email, password_hash, role) VALUES ($1, $2, $3) RETURNING *",
         [email, hashedPassword, role]
       );
@@ -55,3 +55,55 @@ router.post("/register"),
       });
     }
   };
+
+// Login Route
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find User
+    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (user.row.length === 0) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Check Password
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].password_hash
+    );
+
+    if (!validPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    req.session.userId = user.rows[0].id;
+    req.session.userRole = user.rows[0].role;
+
+    res.json({
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user.rows[0].id,
+        email: user.rows[0].email,
+        role: user.rows[0].role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Login failed",
+      error: error.message,
+    });
+  }
+});
+// Logout Route
