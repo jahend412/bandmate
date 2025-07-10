859249a6578cb75d88582f9e5c999a7e7a602096
Josh Endemann <jahend44@gmail.com>
2025-07-10 18:16:41 -0500

    Start the dashboard page for Musician User
    - useState to set the user and a loading screen
    - useEffect to create an async function to fetch the profile from the database.
    - Return a header that says welcome back and the users name

---
+++ frontend/src/app/dashboard/musician/page.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function MusiciansPage({ user }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/profiles/me", {
+          credentials: "include",
+        });
+        const data = await response.json();
+        setUser(data.profile);
+      } catch (error) {
+        console.error("Error fetching profile:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome Back ${user} </h1>
+    </div>
+  );
+}
